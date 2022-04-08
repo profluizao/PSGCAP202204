e4b77573392507f4ff3a5232278cfb3f4da438c7
Prof. Luiz Augusto Rodrigues <90289697+profluizao@users.noreply.github.com>
2022-04-08 16:15:56 -0400

    Create CREATE TABLE Categoria.sql

---
+++ SqlServer/CREATE TABLE Categoria.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Categoria (
+	ID_Categoria INT NOT NULL IDENTITY(1,1),
+	Descricao_Categoria VARCHAR(MAX) NOT NULL,
+	Status BIT NULL DEFAULT 1,
+	DataInclusao DATETIME NULL DEFAULT GETDATE(),
+	CONSTRAINT PK_Categoria PRIMARY KEY (ID_Categoria)
+)
+GO
