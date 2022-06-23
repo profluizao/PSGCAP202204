2bf19412467f2e15f86aefedffacdbe3b79e0d9b
Prof. Luiz Augusto Rodrigues <90289697+profluizao@users.noreply.github.com>
2022-06-23 15:23:37 -0400

    Create INSERT INTO Tipo_Rebanho.sql

---
+++ Rebanho/INSERT INTO Tipo_Rebanho.sql
@@ -0,0 +1,12 @@
+INSERT INTO Tipo_Rebanho(Descricao) VALUES
+('Bovino'),
+('Bubalino'),
+('Caprino'),
+('Codornas'),
+('Equino'),
+('Galináceos - galinhas'),
+('Galináceos - total'),
+('Ovino'),
+('Suíno - matrizes de suínos'),
+('Suíno - total')
+GO
